bb755a93247eed9a676a6774e3d1c37d61ba84ad
T-Alexander <tomalexander83@hotmail.co.uk>
2014-10-28 21:06:15 +0000

    Adds pagination to products page

--- inc/header.php
+++ inc/header.php
@@ -17,7 +17,8 @@
 			<ul class="nav">
 				<li class="shirts <?php if ($section == "shirts") { echo "on"; } ?>"><a href="<?php echo BASE_URL; ?>shirts/">Shirts</a></li>
 				<li class="contact <?php if ($section == "contact") { echo "on"; } ?>"><a href="<?php echo BASE_URL; ?>contact/">Contact</a></li>
-				<li class="about <?php if ($section == "about") { echo "on"; } ?>"><a href="<?php echo BASE_URL; ?>about/">About</a></li>
+				<li class="search <?php if ($section == "search") { echo "on"; } ?>"><a href="<?php echo BASE_URL; ?>search/">Search</a></li>
+				
 				<li class="cart"><a href="#">Shopping Cart</a></li><!-- view treehouse video /php/stage7/deployingthesite/5:00 to activate paypal cart -->
 			</ul>
 
--- shirts/shirts.php
+++ shirts/shirts.php
@@ -2,7 +2,35 @@
 
 	require_once("../inc/config.php");
 	require_once(ROOT_PATH . "inc/products.php"); 
-	$products = get_products_all();
+
+	if(empty($_GET["pg"])) {
+		$current_page = 1;
+	} else {	
+		$current_page = $_GET["pg"];
+	}
+
+	$current_page = intval($current_page);
+
+	$total_products = get_products_count();
+	$products_per_page = 8;
+	$total_pages = ceil($total_products / $products_per_page);
+
+	if ($current_page > $total_pages) {
+		header("Location: ./?pg=" . $total_pages);
+	}
+
+	if ($current_page < 1) {
+		header("Location: ./");
+	}
+
+	$start = (($current_page - 1) * $products_per_page) + 1;
+	$end = $current_page * $products_per_page;
+	if ($end > $total_products) {
+		$end = $total_products;
+	}
+
+	$products = get_products_subset($start,$end);
+
 ?><?php 
 	$pageTitle = "Flock Of Three's Full Catalog";
 	$section = "shirts";
@@ -15,6 +43,8 @@
 
 			<h1>Flock Of Three&rsquo;s Full Catalog Of Clutches</h1>
 
+			<?php include(ROOT_PATH . "inc/partial-list-navigation.html.php"); ?>
+
 			<ul class="products">
 
 				<?php foreach($products as $product) {
@@ -22,6 +52,8 @@
 					}
 				?>
 			</ul>	
+		
+			<?php include(ROOT_PATH . "inc/partial-list-navigation.html.php"); ?>
 		
 		</div>	
 
